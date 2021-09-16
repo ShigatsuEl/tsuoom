d9da69dbd09b22908bfefef42f33e96762ea0b11
MinChan Lee <shigatsu970704@gmail.com>
2021-09-16 15:12:37 +0900

    08. Nicknames Part One

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,24 +1,39 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+const nicknameForm = document.querySelector("#nickname");
+const messageForm = document.querySelector("#message");
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server ✅");
 });
 
 socket.addEventListener("message", (message) => {
-  console.log("New message: ", message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
   console.log("Disconnected from Server ❌");
 });
 
-function handleSubmit(event) {
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+
+function handleMessageSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("message", input.value));
   input.value = "";
 }
 
-messageForm.addEventListener("submit", handleSubmit);
+function handleNicknameSubmit(event) {
+  event.preventDefault();
+  const input = nicknameForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+}
+
+nicknameForm.addEventListener("submit", handleNicknameSubmit);
+messageForm.addEventListener("submit", handleMessageSubmit);
